d02c49d7f3087c554e2dfb9df843bd5b4d568109
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-16 15:07:22 +1000

    Corrected Error on JSON feed

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -10,6 +10,51 @@ app.controller('TankCtrl', [ '$scope', '$http', function($scope, $http){
         })
         .success(function(data){
             $scope.tanks = data.data;
+    console.log($scope.tanks);
+//            var tempImg;
+//            console.log(tempTank);
+
+//            var all = [];
+//            
+//            var val;
+//            for(val in tempTank){
+//                //console.log(val);
+//                
+//                $http({method: 'GET', 
+//                    url: 'http://api.worldoftanks.asia/wot/encyclopedia/tankinfo/', 
+//                    params: {application_id: '840a965aa2a0d19550d6d2a03d63e955', language: 'en', tank_id: val}
+//                })
+//                .success(function(data){
+//                    var tonks = data.data;
+//                    
+//                    var num;
+//                    
+//                    
+//                    for(num in tonks){
+//                        if(tonks.hasOwnProperty(num)){
+//                            var img = tonks[num].image;
+//                            var name = tonks[num].name_i18n;
+//                            var nation = tonks[num].nation_i18n;
+//                            var tier = tonks[num].level;
+//                            var type = tonks[num].type_i18n;
+//                            all.push("{'image' : '"+img+"', 'name_i18n' : '"+name+"', 'nation_i18n' : '"+nation+"', 'level' : '"+tier+"', 'type_i18n' : '"+type+"' }");
+//                        }
+//                    }
+//                    
+//                });
+//                
+//                //$scope.tanks = tempTank;
+//                //$scope.tanks.images = tempImg;
+//                //console.log($scope.tanks);
+//                //console.log($scope.tanks.images);
+//                
+//            }setTimeout(function(){
+//                $scope.tanks = all;
+//                    console.log(all);
+//                    console.log($scope.tanks[1]["image"]);
+//                }, 30000);
+                
+                
         })
         .error(function(){
             alert('Unable to retrieve data at this time. Please try again later.');
