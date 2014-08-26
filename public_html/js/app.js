2459550146e2cd730f4b518cb94b9dfba093b9aa
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-26 13:50:21 +1000

    Added more images and filters

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -4,7 +4,7 @@
 var app = angular.module('tankApp', [
     'ngRoute',
     'ngAnimate',
-    'objFilterModule',
+    'tankFilters',
     'tankControllers'
     ] );
 
--- public_html/js/filters.js
+++ public_html/js/filters.js
@@ -1,9 +1,11 @@
 'use strict';
 
-//  objFilter solution found on stackoverflow - 
-//      Filters can only filter arrays and not objects within an object.
+var tankFilters = angular.module('tankFilters', []);
 
-angular.module('objFilterModule',[]).filter('objFilter', function() {
+
+//      objFilter solution found on stackoverflow - 
+//      Filters can only filter arrays and not objects within an object.
+tankFilters.filter('objFilter', function() {
   return function(items, filter) {
       if (!filter){
           return items;
@@ -18,5 +20,72 @@ angular.module('objFilterModule',[]).filter('objFilter', function() {
           });
         });
         return result;
+    };
+});
+
+tankFilters.filter('tier', function() {
+    return function(input) {
+        switch(input){
+            case 1: return 'images/levels/level_1.png';
+                break;
+            case 2: return 'images/levels/level_2.png';
+                break;
+            case 3: return 'images/levels/level_3.png';
+                break;
+            case 4: return 'images/levels/level_4.png';
+                break;
+            case 5: return 'images/levels/level_5.png';
+                break;
+            case 6: return 'images/levels/level_6.png';
+                break;
+            case 7: return 'images/levels/level_7.png';
+                break;
+            case 8: return 'images/levels/level_8.png';
+                break;
+            case 9: return 'images/levels/level_9.png';
+                break;
+            case 10: return 'images/levels/level_10.png';
+                break;
+        }
+    };
+});
+
+
+tankFilters.filter('flag', function(){
+    return function(input) {
+        switch(input){
+            case "U.S.A.": return 'images/nations/usa.png';
+                break;
+            case "U.S.S.R.": return 'images/nations/ussr.png';
+                break;
+            case "Germany": return 'images/nations/germany.png';
+                break;
+            case "China": return 'images/nations/china.png';
+                break;
+            case "France": return 'images/nations/france.png';
+                break;
+            case "U.K.": return 'images/nations/uk.png';
+                break;
+            case "Japan": return 'images/nations/japan.png';
+                break;
+        }
+    };
+});
+
+
+tankFilters.filter('type', function(){
+    return function(input) {
+        switch(input){
+            case "AT-SPG": return 'images/tanks/AT-SPG.png';
+                break;
+            case "SPG": return 'images/tanks/SPG.png';
+                break;
+            case "mediumTank": return 'images/tanks/mediumTank.png';
+                break;
+            case "lightTank": return 'images/tanks/lightTank.png';
+                break;
+            case "heavyTank": return 'images/tanks/heavyTank.png';
+                break;
+        }
     };
 });
