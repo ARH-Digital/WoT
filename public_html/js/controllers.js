d529a3e7f87d0def0e05cf587a153f1c41e9ab53
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-26 10:33:47 +1000

    Added all modules

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -29,6 +29,7 @@ tankControllers.controller('tankPageCtrl', ['$scope', '$http', function($scope, 
         .success(function(data){
             $scope.tank = data.data[id];
             console.log($scope.tank);
+            
             var turrets = [];
             var num;
             var val;
@@ -41,12 +42,70 @@ tankControllers.controller('tankPageCtrl', ['$scope', '$http', function($scope, 
                     var turret = data.data;
                     turrets.push(turret);
                     $scope.turrets = turrets;
-                    console.log($scope.turrets);
+                });
+            }
+            
+            var guns = [];
+            var num;
+            var val;
+            for(num in $scope.tank.guns){
+                val = $scope.tank.guns[num].module_id;
+                $http({method: 'GET', 
+                    url: 'http://api.worldoftanks.asia/wot/encyclopedia/tankguns/', 
+                    params: {application_id: '840a965aa2a0d19550d6d2a03d63e955', language: 'en', module_id: val}
+                }).success(function(data){
+                    var gun = data.data;
+                    guns.push(gun);
+                    $scope.guns = guns;
+                });
+            }
+            
+            var engines = [];
+            var num;
+            var val;
+            for(num in $scope.tank.engines){
+                val = $scope.tank.engines[num].module_id;
+                $http({method: 'GET', 
+                    url: 'http://api.worldoftanks.asia/wot/encyclopedia/tankengines/', 
+                    params: {application_id: '840a965aa2a0d19550d6d2a03d63e955', language: 'en', module_id: val}
+                }).success(function(data){
+                    var engine = data.data;
+                    engines.push(engine);
+                    $scope.engines = engines;
+                });
+            }
+            
+            var radios = [];
+            var num;
+            var val;
+            for(num in $scope.tank.radios){
+                val = $scope.tank.radios[num].module_id;
+                $http({method: 'GET', 
+                    url: 'http://api.worldoftanks.asia/wot/encyclopedia/tankradios/', 
+                    params: {application_id: '840a965aa2a0d19550d6d2a03d63e955', language: 'en', module_id: val}
+                }).success(function(data){
+                    var radio = data.data;
+                    radios.push(radio);
+                    $scope.radios = radios;
+                });
+            }
+            
+            var chassis = [];
+            var num;
+            var val;
+            for(num in $scope.tank.chassis){
+                val = $scope.tank.chassis[num].module_id;
+                $http({method: 'GET', 
+                    url: 'http://api.worldoftanks.asia/wot/encyclopedia/tankchassis/', 
+                    params: {application_id: '840a965aa2a0d19550d6d2a03d63e955', language: 'en', module_id: val}
+                }).success(function(data){
+                    var chassi = data.data;
+                    chassis.push(chassi);
+                    $scope.chassis = chassis;
                 });
             }
             
         });
-        
 }]);
 
 
